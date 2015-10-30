56fb7ca45c717d4e3a6050537081c35f324aeb3e
kongqw <qingwei.kong@bitmaintech.com>
2015-10-30 18:52:41 +0800

    封装

--- app/src/main/java/kong/qingwei/demo/kqwcheckviewdemo/CheckView.java
+++ app/src/main/java/kong/qingwei/demo/kqwcheckviewdemo/CheckView.java
@@ -7,105 +7,111 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by kongqw on 2015/10/23.
  */
 public class CheckView extends View {
     Context mContext;
-    int[] CheckNum = null;
+    String mCheckCode = null;
     Paint mTempPaint = new Paint();
     private final int mPointNum;
     private final int mLineNum;
     private final int mTextLength;
     private final float mTextSize;
-    private final int mTextColor;
+//    private final int mTextColor;
     private final int mBgColor;
 
     // 验证码
     public CheckView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
-        mTempPaint.setAntiAlias(true);
-        mTempPaint.setTextSize(Config.TEXT_SIZE);
-        mTempPaint.setStrokeWidth(3);
-
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CheckView);
         // 获取随机点的个数
-        mPointNum = a.getInteger(R.styleable.CheckView_point_num, 100);
+        mPointNum = a.getInteger(R.styleable.CheckView_point_num, 0);
         // 获取随机线的条数
-        mLineNum = a.getInteger(R.styleable.CheckView_line_num, 10);
+        mLineNum = a.getInteger(R.styleable.CheckView_line_num, 0);
         // 获取验证码长度
         mTextLength = a.getInteger(R.styleable.CheckView_text_length, 4);
         // 获取验证码字体大小
         mTextSize = a.getDimension(R.styleable.CheckView_text_size, 30);
         // 获取验证码字体颜色
-        mTextColor = a.getColor(R.styleable.CheckView_text_color, 0XFFFFFFFF);
+//        mTextColor = a.getColor(R.styleable.CheckView_text_color, 0XFFFFFFFF);
         // 获取背景颜色
         mBgColor = a.getColor(R.styleable.CheckView_bg_color, 0XFFFFFFFF);
-
-
-        // 获取验证码类型（纯数字/纯字母/混合）
-        Log.d("CheckView", "point_num = " + mPointNum);
-        Log.d("CheckView", "line_num = " + mLineNum);
-        Log.d("CheckView", "text_length = " + mTextLength);
-        Log.d("CheckView", "text_color = " + mTextColor);
-        Log.d("CheckView", "text_size = " + mTextSize);
-        Log.d("CheckView", "bg_color = " + mBgColor);
-
         a.recycle();
+
+        mTempPaint.setAntiAlias(true);
+        mTempPaint.setTextSize(mTextSize);
+        mTempPaint.setStrokeWidth(3);
+//        Log.d("CheckView", "point_num = " + mPointNum);
+//        Log.d("CheckView", "line_num = " + mLineNum);
+//        Log.d("CheckView", "text_length = " + mTextLength);
+//        Log.d("CheckView", "text_color = " + mTextColor);
+//        Log.d("CheckView", "text_size = " + mTextSize);
+//        Log.d("CheckView", "bg_color = " + mBgColor);
     }
 
 
     public void onDraw(Canvas canvas) {
-
-        getCheckNum();
-
+        // 生成验证码
+        mCheckCode = makeCheckCode();
+        // 设置二维码背景色
         canvas.drawColor(mBgColor);
+
         final int height = getHeight();
         // 获得CheckView控件的高度
         final int width = getWidth();
         // 获得CheckView控件的宽度
         int dx = width / mTextLength / 2;
+
+        char[] checkNum = mCheckCode.toCharArray();
         for (int i = 0; i < mTextLength; i++) {
             // 绘制验证控件上的文本
-            canvas.drawText("" + CheckNum[i], dx, getPositon(height), mTempPaint);
+            canvas.drawText("" + checkNum[i], dx, getPositon(height), mTempPaint);
             dx += width / (mTextLength + 1);
         }
         int[] line;
-        for (int i = 0; i < Config.LINE_NUM; i++) {
+        for (int i = 0; i < mLineNum; i++) {
             // 划线
-            line = CheckUtil.getLine(height, width);
+            line = getLine(height, width);
             canvas.drawLine(line[0], line[1], line[2], line[3], mTempPaint);
         }
         // 绘制小圆点
         int[] point;
-        for (int i = 0; i < Config.POINT_NUM; i++) {
+        for (int i = 0; i < mPointNum; i++) {
             // 画点
-            point = CheckUtil.getPoint(height, width);
+            point = getPoint(height, width);
             canvas.drawCircle(point[0], point[1], 1, mTempPaint);
         }
     }
 
-    public void invaliChenkNum() {
+    /**
+     * 生成新的验证码
+     */
+    public void invaliChenkCode() {
         invalidate();
     }
 
+    public String getCheckCode() {
+        return mCheckCode;
+    }
+
     /**
-     * 产生随机数字
+     * 产生随机验证码
      *
      * @return
      */
-    public int[] getCheckNum() {
-        int[] tempCheckNum = new int[mTextLength];
+    public String makeCheckCode() {
+        StringBuffer sb = new StringBuffer();
         for (int i = 0; i < mTextLength; i++) {
-            tempCheckNum[i] = (int) (Math.random() * 10);
+            int temp = (int) (Math.random() * 10);
+            sb.append(temp);
         }
-        CheckNum = tempCheckNum;
-        return tempCheckNum;
+        return sb.toString();
     }
-
 
     /**
      * 计算验证码的绘制y点位置
@@ -115,10 +121,40 @@ public class CheckView extends View {
      */
     public int getPositon(int height) {
         int tempPositoin = (int) (Math.random() * height);
-        if (tempPositoin < 20) {
-            tempPositoin += 20;
+        if (tempPositoin < 50) {
+            tempPositoin += 50;
         }
         return tempPositoin;
     }
 
+
+    /**
+     * 随机产生划线的起始点坐标和结束点坐标
+     *
+     * @param height 传入CheckView的高度值
+     * @param width  传入CheckView的宽度值
+     * @return 起始点坐标和结束点坐标
+     */
+    public static int[] getLine(int height, int width) {
+        int[] tempCheckNum = {0, 0, 0, 0};
+        for (int i = 0; i < 4; i += 2) {
+            tempCheckNum[i] = (int) (Math.random() * width);
+            tempCheckNum[i + 1] = (int) (Math.random() * height);
+        }
+        return tempCheckNum;
+    }
+
+    /**
+     * 随机产生点的圆心点坐标
+     *
+     * @param height 传入CheckView的高度值
+     * @param width  传入CheckView的宽度值
+     * @return
+     */
+    public static int[] getPoint(int height, int width) {
+        int[] tempCheckNum = {0, 0, 0, 0};
+        tempCheckNum[0] = (int) (Math.random() * width);
+        tempCheckNum[1] = (int) (Math.random() * height);
+        return tempCheckNum;
+    }
 }
--- app/src/main/java/kong/qingwei/demo/kqwcheckviewdemo/MainActivity.java
+++ app/src/main/java/kong/qingwei/demo/kqwcheckviewdemo/MainActivity.java
@@ -11,67 +11,43 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private CheckView mCheckView;
-    private TextView mShowPassViwe;
     private EditText mEditPass;
     private Button mSubmit;
     private Button mRef;
     //     验证码：
-    private int[] checkNum = null;
+    private String mCheckCode = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        initCheckNum();
-    }
-
-
-    public void initView() {
+        // 初始化控件
         mCheckView = (CheckView) findViewById(R.id.checkView);
-        mShowPassViwe = (TextView) findViewById(R.id.checkpass);
         mEditPass = (EditText) findViewById(R.id.checkTest);
         mSubmit = (Button) findViewById(R.id.submit);
         mRef = (Button) findViewById(R.id.ref);
         mSubmit.setOnClickListener(this);
-        mRef.setOnClickListener(this
-        );
-    }
+        mRef.setOnClickListener(this);
 
-    // 初始化验证码并且刷新界面
-    public void initCheckNum() {
-//        checkNum = CheckUtil.getCheckNum();
-//        mCheckView.setCheckNum(checkNum);
-        checkNum = mCheckView.getCheckNum();
-        mCheckView.invaliChenkNum();
+        // 生成验证码
+        mCheckView.invaliChenkCode();
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.submit:
-                String userInput = mEditPass.getText().toString();
-                if (CheckUtil.checkNum(userInput, checkNum)) {
-                    setPassString("通过");
+                if (mEditPass.getText().toString().equals(mCheckView.getCheckCode())) {
                     Toast.makeText(this, "通过", Toast.LENGTH_SHORT).show();
                 } else {
-                    setPassString("未通过");
                     Toast.makeText(this, "未通过", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.ref:
-                initCheckNum();
+                // 生成新的验证码
+                mCheckView.invaliChenkCode();
                 break;
             default:
                 break;
-
         }
-
     }
-
-
-    public void setPassString(String passString) {
-        mShowPassViwe.setText(passString);
-    }
-
-
 }
